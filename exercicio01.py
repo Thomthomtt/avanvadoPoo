0e1a57225027ba7b38e5960ec667c1551936a8df
thomthomtt <thomazgt39@gmail.com>
2025-05-06 16:39:46 -0300

    testando funções

---
+++ exercicio01.py
@@ -0,0 +1,16 @@
+from biblioteca import Pessoa
+
+aluno01=Pessoa(130,"Thais Karla", 87)
+aluno02=Pessoa(57,"Taynara Raynara", 23)
+
+print(aluno01.nome)
+print(aluno02.nome)
+aluno01.pararComer()
+aluno01.comer()
+aluno01.pararComer()
+aluno01.comer()
+
+
+
+
+
